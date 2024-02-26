a960fe1435ea3f0ec1d42c3e0fa403bdf5709a52
Gray <cgrichters@gmail.com>
2024-02-26 14:26:40 -0500

    Initialize basic openGL functionality

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 BUILD_DIR=./build
 
+GLAD_FILES=./extern/GLAD/glad.c
 IFLAGS=-I ./extern/include
+LFLAGS=-L ./extern/lib
+LIBS=-l glfw3 -lm
 
-prism: src/main.c
-	gcc $^ -o ${BUILD_DIR}/$@ -I ./extern/include
+prism: src/main.c ${GLAD_FILES}
+	gcc $^ -o ${BUILD_DIR}/$@ ${IFLAGS} ${LFLAGS} ${LIBS}
 
 compile_commands:
 	bear -- make && mv compile_commands.json ./build
--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 
 // Local Lib Includes
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 // Error Logging function, 
@@ -18,11 +19,43 @@ void log_error(const char *format, ...) {
     va_end(args);
 }
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
 int main(int argc, char *argv[])
 {
-    
-    log_error("Caught test error #%d\n", 5);
-    printf("Test\n");
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 800, "Test Window", NULL, NULL);
+
+    if (window == NULL) {
+        log_error("Error: Failed to create GLFW Window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        log_error("Error: failed to initialize GLAD\n");
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 800);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
